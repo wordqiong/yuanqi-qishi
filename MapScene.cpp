d66e08353026a71067b496289df12558dd2e401c
wordqiong <83449899+wordqiong@users.noreply.github.com>
2021-06-06 13:48:48 +0800

    移动最终版 还有地图素材
    存在问题：1.墙的判断 斜着走会跳到墙里卡死 终于知道以前卡cfbug原理了
                       2.人物会偏移中心 待写好安全屋危险屋分类后可解决 无怪人居于中间

--- MapScene.cpp
+++ MapScene.cpp
@@ -1,6 +1,10 @@
 #include "MapScene.h"
+
 #define MAP_WALL 203
+#define MAP_LOBBY 12254
+#define MAP_ROOM 11853
 USING_NS_CC;
+
 
 Scene* MapScene::createScene()
 {
@@ -21,7 +25,7 @@ bool MapScene::init()
     {
         return false;
     }
-    //add map to the scene
+    //½«³öÉúµãÉèÖÃÔÚ´°¿ÚÏÂ
     map = TMXTiledMap::create("map.tmx");
     layer2 = map->getLayer("layer2");
     layer2->setVisible(false);
@@ -32,97 +36,174 @@ bool MapScene::init()
     else
     {
         map->setAnchorPoint(Vec2::ZERO);
-       map->setPosition(0,-32*(100-18) );
+        map->setPosition(0, -32 * (100 - 18));
         addChild(map);
     }
-    //ä¸‹é¢ä¸ºæµ‹è¯•å†…å®¹
-    size = Director::getInstance()->getVisibleSize();
-    sprite = Sprite::create("sprite.png");
-    map->addChild(sprite);
-    sprite->setAnchorPoint(Vec2::ZERO);
-    sprite->setPosition(32*10.0f, 32*92.0f);
-        MapMove();
+    //´´½¨hero£¬½«Ëü·ÅÔÚµØÍ¼ÖÐÑë
+    hero = Sprite::create("sprite.png");
+    map->addChild(hero);
+    hero->setAnchorPoint(Vec2::ZERO);
+    hero->setPosition(32 * 10.0f, 32 * 92.0f);
+    scheduleUpdate();
     return true;
 }
-void MapScene::MapMove()
+
+void MapScene::update(float delta)
 {
+    Node::update(delta);
+    auto leftArrow = EventKeyboard::KeyCode::KEY_A, rightArrow = EventKeyboard::KeyCode::KEY_D,
+        upArrow = EventKeyboard::KeyCode::KEY_W, downArrow = EventKeyboard::KeyCode::KEY_S;
     auto listener = EventListenerKeyboard::create();
-    listener->onKeyPressed = [&](EventKeyboard::KeyCode code, Event* e)
+    listener->onKeyPressed = [=](EventKeyboard::KeyCode keyCode, Event* event)
     {
-        log("key,%d", code);
-        switch (code)
-        {
-            case EventKeyboard::KeyCode::KEY_S:
-                    y += 1;
-                    if (isCanReach(32 * 10 - x * 32, 32 * 92 - y * 32)) 
-                    {
-                        map->setPosition(0 + x * 32, -32 * (100 - 18) + y * 32);
-                        sprite->setPosition(32 * 10 - x * 32, 32 * 92 - y * 32);
-                        log("çºµåæ ‡%d", (32 * 92 - y * 32 -16)/32);
-                    }
-                    else
-                    {
-                        y--;
-                    }
-               
-                break;
-            case EventKeyboard::KeyCode::KEY_D:
-                x -= 1;
-                if (isCanReach(32 * 10 - x * 32, 32 * 92 - y * 32))
-                {
-                    map->setPosition(0 + x * 32, -32 * (100 - 18) + y * 32);
-                    sprite->setPosition(32 * 10.0 - x * 32, 32 * 92.0 - y * 32);
-                }
-                else
-                {
-                    x++;
-                }
-                break;
-            case EventKeyboard::KeyCode::KEY_W:
-                y -= 1;
-                if (isCanReach(32 * 10 - x * 32, 32 * 92 - y * 32))
-                {
-                    map->setPosition(0 + x * 32, -32 * (100 - 18) + y * 32);
-                    sprite->setPosition(32 * 10.0 - x * 32, 32 * 92.0 - y * 32);
-                }
-                else
-                {
-                    y++;
-                }
-                break;
-            case EventKeyboard::KeyCode::KEY_A:
-                x += 1;
-                if (isCanReach(32 * 10 - x * 32, 32 * 92 - y * 32))
-                {
-                    map->setPosition(0 + x * 32, -32 * (100 - 18) + y * 32);
-                    sprite->setPosition(32 * 10.0 - x * 32, 32 * 92.0 - y * 32);
-                }
-                else
-                {
-                    x--;
-                }
-                break;
-            default:
-                break;
-        }
+        keys[keyCode] = true;
+        log("%d", keyCode);
+    };
+
+    listener->onKeyReleased = [=](EventKeyboard::KeyCode keyCode, Event* event)
+    {
+        keys[keyCode] = false;
 
     };
-    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, sprite);
+    int offsetX = 0, offsetY = 0;
+    if (keys[leftArrow])
+    {
+        offsetX = -4;
+        FinalMove(offsetX, offsetY, 'a', 'd');
+    }
+    if (keys[rightArrow])
+    {
+        offsetX = 4;
+        FinalMove(offsetX, offsetY, 'd', 'a');
+
+    }
+    if (keys[upArrow])
+    {
+        offsetY = 4;
+        FinalMove(offsetX, offsetY, 'w', 's');
+
+    }
+    if (keys[downArrow])
+    {
+        offsetY = -4;
+        FinalMove(offsetX, offsetY, 's', 'w');
+    }
+    offsetX = offsetY = 0;
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+
 }
-bool MapScene::isCanReach(float x, float y)
+bool MapScene::isCanReach(float x, float y, int Type_Wall)
 {
     bool result;
-    int mapX = (int)((x - 16) / 32+1);
-    int mapY = (int)(99-(y - 16) / 32);
-    int tileGid = layer2->getTileGIDAt(Vec2(mapX, mapY));
-    log("e:%d\nY:%d,X:%d",tileGid, mapY, mapX);
-    if (tileGid == MAP_WALL)
+    int mapX = (int)((x - 16) / 32 + 1);
+    int mapY = (int)(99 - (y - 16) / 32);
+    if (mapX < 0 || mapX>73 || mapY < 0 || mapY>99)
     {
-        result = false;//ä¸å¯ç§»åŠ¨
+        return false;
+    }
+    int tileGid = layer2->getTileGIDAt(Vec2(mapX, mapY));
+    log("e:%d\nY:%d,X:%d", tileGid, mapY, mapX);
+    if (tileGid == Type_Wall)
+    {
+        result = false;//²»¿ÉÒÆ¶¯
     }
     else
     {
-        result = true;//å¯ç§»åŠ¨
+        result = true;//¿ÉÒÆ¶¯
     }
     return result;
 }
+
+void MapScene::PureMapMove(int offsetX, int offsetY)
+{
+    auto moveTo = MoveTo::create(1.0 / 1000, Vec2(map->getPositionX() - offsetX, map->getPositionY() - offsetY));
+    map->runAction(moveTo);
+}
+void MapScene::PureHeroMove(int offsetX, int offsetY)
+{
+    auto moveTo = MoveTo::create(1.0 / 1000, Vec2(hero->getPositionX() + offsetX, hero->getPositionY() + offsetY));
+    hero->runAction(moveTo);
+}
+void MapScene::AllMove(int offsetX, int offsetY)
+{
+    PureMapMove(offsetX, offsetY);
+    PureHeroMove(offsetX, offsetY);
+}
+
+bool MapScene::JudgeWall(int offsetX, int offsetY, char key_arrow)
+{
+    int i = 1;
+    while (i <= 8)
+    {
+        if (!isCanReach(hero->getPositionX() + offsetX + ('d' == key_arrow) * (1) * (i * 32) + ('a' == key_arrow) * (-1) * (i * 32),
+            hero->getPositionY() + offsetY + ('w' == key_arrow) * (1) * (i * 32) + ('s' == key_arrow) * (-1) * (i * 32), MAP_WALL))
+        {
+
+            log("i=%d", i);
+            break;
+        }
+        else
+            i++;
+
+    }
+
+    if (i <= 5)
+        return true;//Îå¸ñ·¶Î§ÄÚÓÐÇ½
+    else
+        return false;
+}
+void MapScene::FinalMove(int offsetX, int offsetY, char key_arrow_1, char key_arrow_2)
+{
+    if (((JudgeWall(offsetX, offsetY, key_arrow_1)
+        && isCanReach(hero->getPositionX() + offsetX, hero->getPositionY() + offsetY, MAP_WALL))
+        || ((JudgeWall(offsetX, offsetY, key_arrow_2)
+            && isCanReach(hero->getPositionX() + offsetX, hero->getPositionY() + offsetY, MAP_WALL))))
+        && isCanReach(hero->getPositionX() + offsetX+ ('d' == key_arrow_1) * (1 * 16) + ('a' == key_arrow_1) * (-1*16), hero->getPositionY() + offsetY+ ('w' == key_arrow_1) * (1 * 16) + ('s' == key_arrow_1) * (-1 * 16), MAP_WALL))
+    {
+        PureHeroMove(offsetX, offsetY);
+    }
+    else
+    {
+        if (isCanReach(hero->getPositionX() + offsetX + ('d' == key_arrow_1) * (1 * 16) + ('a' == key_arrow_1) * (-1 * 16), hero->getPositionY() + offsetY + ('w' == key_arrow_1) * (1 * 16) + ('s' == key_arrow_1) * (-1 * 16), MAP_WALL))
+        {
+            AllMove(offsetX, offsetY);
+        }
+    }
+}
+//void MapScene::OpenDoor()
+//{
+//    if (StateDoor())
+//    {
+//        //Ïû³ýÌí¼ÓµÄÄÇ¼¸¸ö¾«ÁéÇ½¿é
+//    }
+//
+//}
+//void MapScene::CloseDoor()
+//{
+//    if (!StateDoor())
+//    {
+//        //ÔÚÀ´µÄÂ·ÉÏÌí¼ÓÇ½Ìå
+//    }
+//}
+//bool MapScene::StateDoor()
+//{
+//    //¼Æ»®Ä¿±ê ¶ÁÈ¥µ±Ç°
+//    //true Ôò¿ªÃÅ ´ú±í´òÍê¹Ö ¿ªÃÅ
+//    //false ´Ó×ßÀÈ×ß½ø·¿¼ä
+//    if (PositionDoor)
+//    {
+//        if (!isCanReach(32 * (10 - x), 32 * (92 - y), MAP_LOBBY))
+//        {
+//            PositionDoor = false;
+//            return true;
+//        }
+//    }
+//    else
+//    {
+//        if (!isCanReach(32 * (10 - x), 32 * (92 - y), MAP_ROOM))
+//        {
+//            PositionDoor = true;
+//            return false;
+//        }
+//    }
+//}
--- MapScene.h
+++ MapScene.h
@@ -2,20 +2,22 @@
 #define __MAP_SCENE_H__
 
 #include "cocos2d.h"
-
 class MapScene : public cocos2d::Scene
 {
 public:
     static cocos2d::Scene* createScene();
 
     virtual bool init();
-    //娴嬭瘯鐢ㄧ殑鎴愬憳
-    cocos2d::Size size;
-    cocos2d::Sprite* sprite;
+    //测试用的成员
+   //瓦片初始地图
     cocos2d::TMXTiledMap* map;
+    //障碍物所在图层
     cocos2d::TMXLayer* layer2;
-
-
+    //hero单位
+    cocos2d::Sprite* hero;
+    //移动所需的语句
+    void update(float delta) override;
+    std::map<cocos2d::EventKeyboard::KeyCode, bool> keys;
     CREATE_FUNC(MapScene);
     /*
     *@brief  open doors
@@ -36,14 +38,26 @@ public:
     *@brief  Map move
     *@author wyh
     */
-    void MapMove();
-    int x = 0, y = 0;//膯膽木膬
-  /*
+    int x = 0, y = 0;//起点
+    /*
     *@brief  Judge next movement whether or not barrier
     *@author wyh
     */
-    bool MapScene::isCanReach(float x, float y);
-
+    bool MapScene::isCanReach(float x, float y, int Type_Wall);
+    /*
+    *@brief  Judge the state of door
+    *@author wyh
+    * @return ture Door
+    */
+    bool StateDoor();
+protected:
+    //自用
+    void MapScene::AllMove(int offsetX, int offsetY);
+    void MapScene::PureMapMove(int offsetX, int offsetY);
+    void MapScene::PureHeroMove(int offsetX, int offsetY);
+    bool MapScene::JudgeWall(int offsetX, int offsetY, char key_arrow);
+    void MapScene::FinalMove(int offsetX, int offsetY, char key_arrow_1, char key_arrow_2);
+    int MonsterNum;
+    bool PositionDoor = true;//in room ->ture in lobby->false 
 };
-
 #endif 
