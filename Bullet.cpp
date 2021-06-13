939b3f686f72beac342092e1967ec6bdfb405341
KKKA404 <80148918+KKKA404@users.noreply.github.com>
2021-06-13 16:43:03 +0800

    新增子弹方向和随机偏移

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,78 +1,33 @@
-#include"Gun.h"
-#include "cocos2d.h"
+#include "Bullet.h"
+#include<ctime>
+#include<cmath>
 
-USING_NS_CC;
-
-bool Gun::init() {
+bool Bullet::init() {
 	if (!Entity::init()) {
 		return false;
 	}
-	is_fire = false;//没开火
-	this->schedule(CC_SCHEDULE_SELECTOR(Gun::myupdate),0.20);//不知道咋回事
+
 	return true;
-}
-
-//这个函数参数可以传枪支的坐标，这样让子弹的初始坐标在枪支前面一点。
-void Gun::createBullets(Point X_Y_of_Gun,Point direction_vector) {
-	Bullet* bullet = Bullet::create(); 
-	
-	bullet->bindSprite(Sprite::create("fireBullet.png"));
-	int y = (int)direction_vector.y; int x = (int)direction_vector.x; int L = x * x + y * y;
-	int s = (int)sqrt((double)(L));
-	
-	unsigned seed = time(0);
-	srand(seed);
-	float f = (float)(rand() % bullet->Bullet_accuracy + 7) / 10;
-	log("%f", f);
-	bullet->numx = (f)*x;
-	bullet->numy = y;
-
-	bullet->getSprite()->setPosition(Vec2((float)((int)X_Y_of_Gun.x + 45 * (int)(direction_vector.x) / s), (float)(5 + (int)X_Y_of_Gun.y + 45 * (int)(direction_vector.y) / s)));//设置子弹的初始位置
-	
-	float radians = atan2(-direction_vector.y, direction_vector.x);//将弧度转换成角度
-	float degree = CC_RADIANS_TO_DEGREES(radians);
-	bullet->getSprite()->setRotation(degree);
-
-	this->addChild(bullet);//显示出子弹
-	
-	this->BulletsVector.pushBack(bullet);//把创建的子弹插到自己的vector中
 	
 }
 
-//void Gun::Bullet_direction() {
-//
-//
+//这是暂定的x方向子弹
+void Bullet::MovebyLine() {
+	
+	this->S = (int)sqrt((float)(numx * numx) + (float)(numy * numy));
+	
+	this->getSprite()->setPositionX(this->getSprite()->getPositionX() + 20 * numx / S);
+	this->getSprite()->setPositionY(this->getSprite()->getPositionY() + 20 * numy / S);
+}
+	
+////再写个根据小怪位置定向的MovebyLine函数,传入方向参数X和Y和速度参数V,此函数暂定！！！
+
+//void Bullet::MovebyLine(int X,int Y,int V) {
+//	this->getSprite()->setPositionX(this->getSprite()->getPositionX() + (V * X / (X * X + Y * Y)));
+//	this->getSprite()->setPositionY(this->getSprite()->getPositionY() + (V * Y / (X * X + Y * Y)));
 //}
 
-void Gun::Fire() {
-	this->createBullets(this->getSprite()->getPosition(),this->shootVector);
+//子弹消失函数，诶这个应该写在Gun类里，直接将满足消失条件的子弹从BulletsVector中移除即可
+void Bullet::Fade() {
 
-}
-
-//传角度旋转
-void Gun::revolve(float degree) {
-
-	/*this->getSprite()->runAction(RotateTo::create(0.5f, degree));*/
-	this->getSprite()->setRotation(degree);//????? ?????  ???? ???????? ?? ?  ?? ???  ?????
-}
-
-//锁定最近敌人算出角度
-float Gun::bindEnemy(Monster* monster1) {
-	//射击方向向量
-	
-		
-			this->shootVector= monster1->getSprite()->getPosition() - this->getSprite()->getPosition();
-			float radians = atan2(-shootVector.y, shootVector.x);
-			/*float s = sqrt((shootVector.x * shootVector.x) + (shootVector.y * shootVector.y));
-			log("%d", shootVector.x);*/
-			//将弧度转换成角度
-			float degree = CC_RADIANS_TO_DEGREES(radians);
-			return degree;
-	
-}
-
-void Gun::myupdate(float dt) {
-	if (this->is_fire) {
-		this->Fire();
-	}
 }
