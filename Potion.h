1befdf0a1b1be1eb3f2bdd309b78c8f113859d81
KKKA404 <80148918+KKKA404@users.noreply.github.com>
2021-06-14 16:26:38 +0800

    药水

---
+++ Potion.h
@@ -0,0 +1,25 @@
+
+#ifndef _POTION_H__
+#define _POTION_H__
+#include<iostream>
+#include<string>
+#include "Entity.h"
+using namespace std;
+
+class Potion : public Entity {
+public:
+	virtual bool init();
+
+	CREATE_FUNC(Potion);
+
+	int bloodRecover = 2;//»Ø¸´ÉúÃüÖµ
+	int type = 1;
+	bool is_can_be_used = true;
+	string load1 = "Potion";
+	string load3 = ".png";
+	string final_load;
+	
+};
+
+
+#endif // !_POTION_H__
